0da1fc39943c26089a00c043c4c4724b877bfbed
Clampari <claraanacaratte1@gmail.com>
2024-11-11 14:38:26 -0300

    Se realizo un nuevo repositorio para guardar hooks personalizados

---
+++ src/HooksApp.jsx
@@ -0,0 +1,29 @@
+import { CalculoPesadoComponent } from "./components/CalculoPesadoComponent"
+import { CallBackComponent } from "./components/CallBackComponent"
+import { CounterComponent } from "./components/CounterComponent"
+import { FetchComponent } from "./components/FetchComponent"
+import { FormsComponent } from "./components/FormsComponent"
+import { ListaTareasComponent } from "./components/ListaTareasComponent"
+
+
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Aplicación de Hooks</h1>
+      <hr />
+      <FetchComponent />
+      <hr />
+      <CounterComponent />
+      <hr />
+      <FormsComponent />
+      <hr />
+      <CalculoPesadoComponent /> 
+      <hr />
+      <CallBackComponent/>
+      
+      <hr />
+      <ListaTareasComponent />
+
+    </>
+  )
+}
